c189997ccecb57d635f961d7c541822627abd037
JS Park <jsp@hashicorp.com>
2021-01-29 12:29:20 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "vault_addr" {
+  type        = string
+  description = "Vault Server Address  for configuration"
+}
+
+variable "ssh_srv_public_ip" {
+  type        = string
+  description = "ssh_srv_public_ip Address  for configuration"
+}
